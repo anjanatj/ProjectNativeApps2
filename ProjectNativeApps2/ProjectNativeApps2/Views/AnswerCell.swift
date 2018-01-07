d5385999a827bedb66e38590f6201f9c483b5049
Anjana T'Jampens <anjanatj@MacBook-Air-van-Anjana.local>
2018-01-07 14:07:09 +0100

    edit layout

--- ProjectNativeApps2/ProjectNativeApps2/Views/AnswerCell.swift
+++ ProjectNativeApps2/ProjectNativeApps2/Views/AnswerCell.swift
@@ -6,8 +6,13 @@ class AnswerCell: UITableViewCell {
     
     var answer: Answer! {
         didSet {
+            self.layer.borderColor = UIColor.gray.cgColor
+            self.layer.borderWidth = 1.0
+            self.layer.cornerRadius = 10
+            
             nameLabel.text = answer.text
         }
+        
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
